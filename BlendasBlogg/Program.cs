c2965397a53ba369184723b5da41dc13437d7f19
Ida Johansson <h25idjoh@du.se>
2025-12-18 09:57:51 +0100

    Första commit
    Skriver in de första kommentarerna för program-klassen

--- BlendasBlogg/Program.cs
+++ BlendasBlogg/Program.cs
@@ -8,6 +8,13 @@
 
             //switch-sats för att hantera användarens val utifrån en meny
 
+            //case 1: visa alla inlägg inom en kategori (i senare steg får man välja kategori)
+
+            //case 2: Söka inlägg
+
+            //case 3: logga in som admin
+
+            //case 4: avsluta programmet
         }
     }
 }
