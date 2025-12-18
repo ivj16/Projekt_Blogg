a52cec693e231992ec8a7ba450593f6fae2f62e9
Ida Johansson <h25idjoh@du.se>
2025-12-18 09:46:02 +0100

    Add files via upload

---
+++ BlendasBlogg/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BlendasBlogg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while-loop som skriver ut menyn tills användaren väljer att avsluta
+
+            //switch-sats för att hantera användarens val utifrån en meny
+
+        }
+    }
+}
