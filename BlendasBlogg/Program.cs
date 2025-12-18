15b668bfca9814804b9066613353bba3fb9d4d1d
Ida Johansson <h25idjoh@du.se>
2025-12-18 10:53:30 +0100

    Add menu structure and placeholders to Program.cs
    Introduced initial menu structure for user and admin interactions in the blog application. Added placeholders for main menu, user menu, admin menu, and search functionality, along with descriptive comments to outline future implementation steps.

--- BlendasBlogg/Program.cs
+++ BlendasBlogg/Program.cs
@@ -4,56 +4,95 @@
     {
         static void Main(string[] args)
         {
-            //while-loop som skriver ut menyn tills användaren väljer att avsluta
+            Console.WriteLine("************ TITEL ************");
+            Console.WriteLine("En blogg av Blenda blogger, för dig som vill veta så mycket onödigt som möjligt");
 
-            //switch-sats för att hantera användarens val utifrån en meny
+            bool isRunAgaing;
 
+        //while-loop som skriver ut menyn tills användaren väljer att avsluta
 
-            //Huvudmeny
-            //case 1: Använda bloggen som användare
+        //switch-sats för att hantera användarens val utifrån en meny
+            switch(x)
+            {
+
+                //Huvudmeny
+                //case 1: Använda bloggen som användare
+                case 1:
+                    break;
+
+//**************************************************************************************************************************************************************************
+                    //Användarmeny
+                    switch ()
+                    { 
+                    // case 1: Lista alla inlägg
+                        case 1:    
+                            //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+                             break;
+
+                        // case 2: Lista alla inlägg utifrån kategori
+                        case 2:
+                            //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+                             break;
+
+                        //case 3: Söka efter inlägg
+                        case 3:
+                            //Inläggssök-meny. Användaren får välja vilket sätt den vill söka på:
+                            switch()
+                            {
+                                //case 1: Sök efter rubrik
+                                case 1: 
+                        
+                                //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+                                    break;
+
+                                case 2: 
+                                //case 2: Sök efter kategori
+                                //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+                                    break;
+
+                                //case 3: Sök efter fritext
+                                case 3:
+                                //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+                                    break;
+                        }
+                            break;
+                    }
+//**************************************************************************************************************************************************************************
+
                 
-                //Användarmeny
-                // case 1: Lista alla inlägg
-                    //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+        //case 2: Logga in som admin
+                case 2:
+                    break;
+//**************************************************************************************************************************************************************************
 
-                // case 2: Lista alla inlägg utifrån kategori
-                    //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
+            //Admin-meny
+                //case 1: Skapa nytt inlägg
+                //case 2: Redigera befintligt inlägg eller ta bort kommentarer
+                    //Lista alla inlägg med ID
+                    //Välj inlägg att redigera
+                    //Redigeringsmeny
+                        //case 1: Redigera rubrik
+                        //case 2: Redigera kategori
+                        //case 3: Redigera innehåll
+                        //case 4: Redigera header
+                        //case 5: Ta bort kommentar
+                            //Lista alla kommentarer med ID
+                            //Välj kommentar att ta bort
+                        //case 6: Gå tillbaka till admin-menyn
+                
+                //case 3: Ta bort inlägg
 
-                //case 3: Söka efter inlägg
-                    //Inläggssök-meny. Användaren får välja vilket sätt den vill söka på:
-                    //case 1: Sök efter rubrik
-                        //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
-
-                    //case 2: Sök efter kategori
-                        //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
-
-                    //case 3: Sök efter fritext
-                        //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
-
-
-            //case 2: Logga in som admin
-                //Admin-meny
-                    //case 1: Skapa nytt inlägg
-                    //case 2: Redigera befintligt inlägg eller ta bort kommentarer
-                        //Lista alla inlägg med ID
-                        //Välj inlägg att redigera
-                        //Redigeringsmeny
-                            //case 1: Redigera rubrik
-                            //case 2: Redigera kategori
-                            //case 3: Redigera innehåll
-                            //case 4: Ta bort kommentar
-                                //Lista alla kommentarer med ID
-                                //Välj kommentar att ta bort
-                            //case 5: Gå tillbaka till admin-menyn
-
-                    //case 3: Ta bort inlägg
-                        //Liknande upplägg som för användare när inlägg listas
-                        //Utifrån ID ska admin kunna ta bort inlägg eller gå tillbaka till admin-menyn
+                    //Liknande upplägg som för användare när inlägg listas
+                    //Utifrån ID ska admin kunna ta bort inlägg eller gå tillbaka till admin-menyn
+//**************************************************************************************************************************************************************************
 
 
 
 
-            //case 3: avsluta programmet
+        //case 3: avsluta programmet
+                case 3:
+                    break;
+            }
         }
     }
 }
