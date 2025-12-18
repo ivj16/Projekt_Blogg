41a3bdbaf019ce53df9b5b3564b424832e88aa56
Ida Johansson <h25idjoh@du.se>
2025-12-18 15:20:26 +0100

    Add UserInfo struct and admin login logic
    Introduced a new UserInfo struct to manage admin credentials and updated Program.cs to prompt for username and password when logging in as admin. Added logic to verify admin credentials and display appropriate messages based on authentication result.

--- BlendasBlogg/Program.cs
+++ BlendasBlogg/Program.cs
@@ -5,57 +5,65 @@
         static void Main(string[] args)
         {
             Console.WriteLine("************ TITEL ************");
-            Console.WriteLine("En blogg av Blenda blogger, för dig som vill veta så mycket onödigt som möjligt");
+            Console.WriteLine("En blogg av Blenda blogger, för dig som vill veta så mycket onödigt som möjligt\n");
 
             bool isRunAgain = true;
+            string userChoice;
 
         //while-loop som skriver ut menyn tills användaren väljer att avsluta
 
-        //switch-sats för att hantera användarens val utifrån en meny
+            //switch-sats för att hantera användarens val utifrån en meny
             while (isRunAgain)
             {
-                Console.WriteLine("Välj ett av nedan val för det som du önskar göra idag:");
-                Console.WriteLine("1, Jag är en användare som vill läsa bloggen \n2, Jag vill logga in som admin och få full kontroll över bloggen \n3, Jag har läst nog för idag och vill säga hejdå");4
+                Console.WriteLine("Välj ett av nedan val för det som du önskar göra idag:\n");
+                Console.WriteLine("1, Jag är en användare som vill läsa bloggen \n2, Jag vill logga in som admin och få full kontroll över bloggen \n3, Jag har läst nog för idag och vill säga hejdå\n");
                 Console.Write("Skriv den siffra som motsvara ditt val: ");
-                string userChoice = Console.ReadLine();
+                userChoice = Console.ReadLine();
 
                 switch (userChoice)
                 {
                     //Huvudmeny
                     //case 1: Använda bloggen som användare
                     case "1":
-    //**************************************************************************************************************************************************************************
+                        //**************************************************************************************************************************************************************************
                         //användarmeny
-                        switch (x)
+                        Console.WriteLine("************************\n");
+                        Console.WriteLine("Så kul att du hittat hit! Det finns massor av kul att läsa om och kommentera gärna och dela med dig av dina historier\n\n");
+                        Console.WriteLine("Vad är du intresserad av att läsa om idag\n?");
+                        Console.WriteLine("1, Jag vill läsa alla inlägg från alla kategorier! \n2, Lista alla inlägg utifrån en kategori \n3, JAg vill söka efter ett inlägg");
+                        Console.Write("Skriv den siffra som motsvara ditt val: ");
+                        userChoice = Console.ReadLine();
+                        
+                        switch (userChoice)
                         { 
                         // case 1: Lista alla inlägg
-                            case 1:    
+                            case "1":    
                                 //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
                                  break;
 
                             // case 2: Lista alla inlägg utifrån kategori
-                            case 2:
+                            case "2":
                                 //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
                                  break;
 
                             //case 3: Söka efter inlägg
-                            case 3:
+                            case "3":
                                 //Inläggssök-meny. Användaren får välja vilket sätt den vill söka på:
-                                switch(x)
+                                switch(userChoice)
                                 {
                                     //case 1: Sök efter rubrik
-                                    case 1: 
+                                    case "1": 
                         
                                     //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
                                         break;
 
                                     //case 2: Sök efter kategori
-                                    case 2: 
+                                    case "2": 
                                     //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
                                         break;
 
                                     //case 3: Sök efter fritext
-                                    case 3:
+                                    case "3":
                                     //Utifrån ID ska användaren kunna välja att gilla eller kommentera, alternativt gå tillbaka till huvudmenyn
                                         break;
                                 }
@@ -67,43 +75,64 @@
 
                     //case 2: Logga in som admin
                     case "2":
+                        UserInfo user = new UserInfo();
+                        Console.WriteLine("************************\n");
+                        Console.WriteLine("Välkommen Blenda! Eller är det verkligen du??\n\n");
+                        Console.Write("Skriv in ditt användarnamn: ");
+                        user.Username = Console.ReadLine(); 
+                        Console.Write("Skriv in ditt lösenord: ");
+                        user.Password = Console.ReadLine();
                         //IF-sats för att kolla användarnamn och lösenord
-
-     //**************************************************************************************************************************************************************************
-                    
-                        //Admin-meny
-                        switch ()
+                        if (user.Username == user.BlendasUsername && user.Password == user.BlendasPassword)
                         {
-                            //case 1: Skapa nytt inlägg
-                            case 1:
-                                //anropa metod från Post-klassen för att skapa nytt inlägg
-                                break;
 
-                            //case 2: Redigera befintligt inlägg eller ta bort kommentarer
-                            case 2:
-                                //Lista alla inlägg med ID - via metod från Post-klassen
-                                //Ta input från användaren för att välja inlägg att redigera
-                                //Anropa redigera inlägg-metoden från Post-klassen
-                                break;
-                  
-                            //case 3: Ta bort inlägg
-                            case 3:
 
-                                //Liknande upplägg som för användare när inlägg listas - metod från Post-klassen.
 
-                                //if-else-sats för att välja ta bort eller gå tillbaka till admin-menyn
-                                break;
+
+                            //**************************************************************************************************************************************************************************
+
+                            //Admin-meny
+                            switch (userChoice)
+                            {
+                                //case 1: Skapa nytt inlägg
+                                case "1":
+                                    //anropa metod från Post-klassen för att skapa nytt inlägg
+                                    break;
+
+                                //case 2: Redigera befintligt inlägg eller ta bort kommentarer
+                                case "2":
+                                    //Lista alla inlägg med ID - via metod från Post-klassen
+                                    //Ta input från användaren för att välja inlägg att redigera
+                                    //Anropa redigera inlägg-metoden från Post-klassen
+                                    break;
+
+                                //case 3: Ta bort inlägg
+                                case "3":
+
+                                    //Liknande upplägg som för användare när inlägg listas - metod från Post-klassen.
+
+                                    //if-else-sats för att välja ta bort eller gå tillbaka till admin-menyn
+                                    break;
+                            }
+                            break;
                         }
-                    break;
+                        else
+                        {
+                            Console.WriteLine("Du är inte Blenda, vänligen gå in som användare i stället.");
+                            Console.WriteLine("PS. Om du faktiskt är Blenda så skrev du in fel inloggningsuppgifter. Försök igen!");
+                        }
+                        break;
     //**************************************************************************************************************************************************************************
 
 
 
 
-            //case 3: avsluta programmet
+                            //case 3: avsluta programmet
                     case "3":
-                        break;
-                }
+                                Console.WriteLine("Hejdå!");
+                                isRunAgain = false;
+                                break;
+                            }
             }
         }
     }
---
+++ BlendasBlogg/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlendasBlogg
+{
+    public struct UserInfo
+    {
+        public UserInfo() { }
+        private string blendasUsername = "BlendaIsCool123";
+        private string blendasPassword = "Stampe4ever<3";
+        public string BlendasUsername { get { return blendasUsername; } }  
+        public string BlendasPassword { get { return blendasPassword; } }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
