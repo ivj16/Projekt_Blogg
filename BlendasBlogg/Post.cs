53dd5adcae977c170a0c9429735967b1eea886f0
Ida Johansson <h25idjoh@du.se>
2026-01-13 11:48:39 +0100

    Flyttar frågetecken
    Flyttar frågetecknet i ett av de hårdkodade inläggen till utanför citationstecknen

--- BlendasBlogg/Post.cs
+++ BlendasBlogg/Post.cs
@@ -73,7 +73,7 @@ namespace BlendasBlogg
         public void CreateDefaultPosts()
         {
             PostList.Add(new Post(1, "Välkommen till Blendas Blogg!", "Hej och välkommen till min blogg! Här kommer jag att dela med mig \nav mina tankar, idéer och äventyr. Hoppas du gillar det!", Category.Nyheter, 1, 5, 0));
-            PostList.Add(new Post(4, "Ordspråk med mumsig twist!", "Brukar du säga \"Som man bäddar får man ligga?\" Då tycker jag du \nkan lägga till en fika-twist och istället säga \n\"Som man bakar får man fika!\" :D", Category.Ordspråk, 2, 0, 0));
+            PostList.Add(new Post(4, "Ordspråk med mumsig twist!", "Brukar du säga \"Som man bäddar får man ligga\"? Då tycker jag du \nkan lägga till en fika-twist och istället säga \n\"Som man bakar får man fika!\" :D", Category.Ordspråk, 2, 0, 0));
             PostList.Add(new Post(0, "Du är snabbare än du tror!", "I alla fall när du är sjuk... Visste du att nysningar kan nå \nupp till 160 km/h i hastighet?! Det är snabbare än en \nkategori 1 orkan!", Category.Roliga_Fakta, 3, 0, 0));
         }
 
